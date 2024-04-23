aebccddad3878d098bba89bac77ec449b2b0247c
chagchagchag <chagchagchag.dev@gmail.com>
2024-04-23 23:10:04 +0900

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/chagchagchag/docs-spring-cloud-data-flow',
+  },
+  docsRepositoryBase: 'https://github.com/chagchagchag/docs-spring-cloud-data-flow',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
